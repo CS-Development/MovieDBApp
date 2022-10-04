1163322516fe1a833316ea4d3e2f73e366307503
Christian Slanzi <christian.slanzi@gmail.com>
2022-10-04 20:25:44 +0200

    basic layout of movie details view

--- MyMovieDB/MyMovieDB/Presentation/MovieDetailView.swift
+++ MyMovieDB/MyMovieDB/Presentation/MovieDetailView.swift
@@ -11,7 +11,72 @@ struct MovieDetailView: View {
     let movie: Movie
     
     var body: some View {
-        Text(movie.titleWithLanguage)
+        ZStack(alignment: .top) {
+            backgroundView
+                .edgesIgnoringSafeArea(.all)
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(alignment: .leading) {
+                    headerView
+                        .padding(.bottom, 20)
+                    moviePosterView
+                    movieOverview
+                    // reviews
+                    // cast
+                    Spacer()
+                }
+                //.padding(.top, 84)
+                .padding(.horizontal, 32)
+            }
+            .padding(.horizontal, 32)
+        }
+    }
+    
+    private var backgroundView: some View {
+        moviePoster
+            .blur(radius: 50)
+    }
+    
+    private var headerView: some View {
+        VStack(alignment: .leading) {
+            Text(movie.titleWithLanguage)
+                .font(.title)
+            
+            Text("Release Date: \(movie.release_date ?? "nd")")
+                .font(.subheadline)
+        }
+        .foregroundColor(.black)
+    }
+    
+    private var moviePosterView: some View {
+        HStack(alignment: .center) {
+            Spacer()
+            moviePoster
+                .frame(width: 200, height: 320)
+            Spacer()
+        }
+    }
+    
+    private var movieOverview: some View {
+        Text(movie.overview ?? "_")
+            .font(.body)
+            .foregroundColor(.black)
+            .padding(.top, 16)
+    }
+    
+    
+    
+    // TODO: replace with better solution
+    private var moviePoster: some View {
+        AsyncImage(url: URL(string: movie.posterPath)) { image in
+            image.resizable().scaledToFill()
+        } placeholder: {
+            ProgressView()
+        }
+        
+        .animation(.easeInOut(duration: 0.5))
+        .transition(.opacity)
+        .cornerRadius(20)
+        .shadow(radius: 15)
     }
 }
 
