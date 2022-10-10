0b5e72e9efa69e7caa30d461a5c6d3788e6f6f14
Christian Slanzi <christian.slanzi@gmail.com>
2022-10-10 20:59:06 +0200

    implemented an ImageLoader with caching capabilities

---
+++ MyMovieDB/MyMovieDB/Presentation/ImageCache/ImageCacheKey.swift
@@ -0,0 +1,19 @@
+//
+//  ImageCacheKey.swift
+//  MyMovieDB
+//
+//  Created by Christian Slanzi on 10.10.22.
+//
+
+import SwiftUI
+
+struct ImageCacheKey: EnvironmentKey {
+    static let defaultValue: ImageCache = TemporaryImageCache()
+}
+
+extension EnvironmentValues {
+    var imageCache: ImageCache {
+        get { self[ImageCacheKey.self] }
+        set { self[ImageCacheKey.self] = newValue }
+    }
+}
